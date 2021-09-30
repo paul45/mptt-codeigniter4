c4d3e8d276b0aeb35ba99d95dc520001365fa4ef
pol_taf <paul.tallet.pinet@free.fr>
2021-09-30 11:33:07 +0200

    init

---
+++ src/Models/MpttModel.php
@@ -0,0 +1,11 @@
+<?php namespace MpttCodeigniter4\Models;
+
+//use CodeIgniter\Model;
+
+class MpttModel //extends Model
+{    
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
